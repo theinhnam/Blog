79d4d0fe4583b7125c90f19c0c50ab8b77916ef6
Nguyen Duy Thanh Nam <nguyenduythanhnam24@gmail.com>
2024-03-27 15:43:58 +0700

    UPDATE Comment

--- src/main/java/com/blog/blog/Controller/DashboardController.java
+++ src/main/java/com/blog/blog/Controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.blog.blog.Controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DashboardController {
 
     @GetMapping("/dashboard")
-    public String displayDashboard(){
+    public String displayDashboard(HttpSession session, Authentication authentication){
+        session.setAttribute("inLoggedUser", authentication.getName());
         return "dashboard.html";
     }
 }
--- src/main/java/com/blog/blog/Controller/LoginController.java
+++ src/main/java/com/blog/blog/Controller/LoginController.java
@@ -2,6 +2,7 @@ package com.blog.blog.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
